4f16c5ba25464b787a9809f5a65d719b98bc8e68
JiriDavid <jiridavidpromise@gmail.com>
2024-06-14 08:59:03 +0530

    completed the navigation and added responsiveness

--- js/main.javascript.js
+++ js/main.javascript.js
@@ -3,3 +3,44 @@ const dropdownContent = document.getElementById("dropdown-content");
 dropdownContent.addEventListener("click", () => {
   dropdownContent.style.visibility = "hidden";
 });
+
+const hamburger = document.getElementById("hamburger");
+const cancel = document.getElementById("cancel");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.add("active");
+  hamburger.style.display = "none";
+  cancel.style.display = "block";
+});
+
+cancel.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  hamburger.style.display = "block";
+  cancel.style.display = "none";
+});
+
+function adjustIconVisibility() {
+  if (window.innerWidth < 768) {
+    navLinks.classList.remove("active");
+    hamburger.style.display = "block";
+    cancel.style.display = "none";
+  } else {
+    navLinks.classList.remove("active");
+    hamburger.style.display = "none";
+    cancel.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", adjustIconVisibility);
+adjustIconVisibility();
+
+const mainNav = document.getElementById("main-menu");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 150) {
+    mainNav.classList.add("fixed");
+  } else {
+    mainNav.classList.remove("fixed");
+  }
+});
